371432db1ec79a33ff687392760268a03946bb29
Daniel Tré <daniel.tre@gmail.com>
2020-05-31 15:52:43 -0300

    Added graphql handler

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"community-api/schemas"
 	"fmt"
 	"net/http"
+
+	"github.com/graphql-go/handler"
 )
 
 func main() {
+	schema, _ := schemas.New()
+
+	http.Handle("/graphql", handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	}))
 	http.HandleFunc("/liveness", func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "OK")
