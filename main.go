cadb3a2e606c37f100b108e8bd801f61eef7603f
Daniel Tré <daniel.tre@gmail.com>
2020-05-31 15:34:07 -0300

    Added /liveness

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/liveness", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "OK")
+	})
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
