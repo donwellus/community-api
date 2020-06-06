334c845211d1b3ad6c30c6c47255f8e77399096b
Daniel Tré <daniel.tre@gmail.com>
2020-06-06 19:09:34 -0300

    Create TopicRepository interface

---
+++ repositories/topic.go
@@ -0,0 +1,16 @@
+package repositories
+
+// TopicCode represents de code for a Topic
+type TopicCode string
+
+// Topic contains information about one topic
+type Topic struct {
+	Code TopicCode `json:"code"`
+	Name string    `json:"name"`
+}
+
+// TopicRepository interface
+type TopicRepository interface {
+	List() []Topic
+	Get(code TopicCode) (Topic, error)
+}
